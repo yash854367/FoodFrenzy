6e6aa5d6aa30aef4ba63dc120013c78779daec67
Yash Baberwal <yash147301@gmail.com>
2025-04-17 15:13:33 +0530

    updated

--- foodFrenzy/src/main/java/repositories/AdminRepository.java
+++ foodFrenzy/src/main/java/repositories/AdminRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import entities.Admin;
 
-public interface AdminRepository extends CrudRepository<Admin, Integer>
-{
+public interface AdminRepository extends CrudRepository<Admin, Integer> {
 	public Admin findByAdminEmail(String email);
 }
---
+++ foodFrenzy/src/main/java/repositories/OrderRepository.java
@@ -0,0 +1,13 @@
+package repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entities.Orders;
+import entities.User;
+
+public interface OrderRepository extends JpaRepository<Orders, Integer>{
+	List<Orders> findOrdersByUser(User user);
+
+}
