6758e3bd8f47d648ee3247d408b649c8272884eb
Yash Baberwal <yash147301@gmail.com>
2025-04-17 14:07:13 +0530

    initial commit

---
+++ foodFrenzy/src/main/java/com/example/foodFrenzy/FoodFrenzyApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodFrenzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFrenzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFrenzyApplication.class, args);
+	}
+
+}
---
+++ foodFrenzy/src/test/java/com/example/foodFrenzy/FoodFrenzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodFrenzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFrenzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
