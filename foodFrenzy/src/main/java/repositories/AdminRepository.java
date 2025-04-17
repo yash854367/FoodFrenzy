7103d2ea279f0cd6ba79a61fb2d2321e339077aa
Yash Baberwal <yash147301@gmail.com>
2025-04-17 15:07:55 +0530

    updated

---
+++ foodFrenzy/src/main/java/repositories/AdminRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import entities.Admin;
+
+public interface AdminRepository extends CrudRepository<Admin, Integer>
+{
+	public Admin findByAdminEmail(String email);
+}
